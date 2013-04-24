be8e179b5ce85c25eff1927d7e32b71c053d730b
Ed Schmalzle <ed@nerded.net>
2013-04-23 20:50:23 -0400

    code formatting

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -2,7 +2,8 @@ class BarsController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.json { render json: Bar.all.to_json(include: [:beers]) }
+      format.json { render json: Bar.all.to_json(only: [:id, :name], 
+                                                 include: [:beers]) }
     end
   end
 end
